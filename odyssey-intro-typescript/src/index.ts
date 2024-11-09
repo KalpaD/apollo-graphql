101badb80b2f68f1bc6fd5d11295582ad610ec5b
Kalpa Senanayake <kalpa.senanayake@gmail.com>
2024-11-09 14:55:04 +1100

    adding basic graph example

--- odyssey-intro-typescript/src/index.ts
+++ odyssey-intro-typescript/src/index.ts
@@ -1,1 +1,43 @@
-// TODO
+import {ApolloServer} from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone";
+import {readFileSync} from "fs";
+import path from "path";
+import {gql} from "graphql-tag"
+import {DocumentNode} from "graphql";
+import {resolvers} from "./resolvers";
+import {ListingAPI} from "./datasources/listing-api";
+
+
+const typeDefs: DocumentNode = gql(
+    readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+        encoding: "utf-8",
+    })
+);
+
+async function startApolloServer() {
+    const server = new ApolloServer({typeDefs, resolvers});
+
+    const {url} = await startStandaloneServer(server, {
+        context: async () => {
+            // To take advantage of the RESTDataSource's caching capabilities,
+            // we need to pass in the server's cache to our instance of ListingAPI.
+            const {cache} = server;
+            return {
+                dataSources: {
+                    listingAPI: new ListingAPI({cache}),
+                },
+            };
+        },
+    });
+    console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+    `)
+}
+
+startApolloServer();
+
+
+
+
+
